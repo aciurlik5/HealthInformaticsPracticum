8cf7b7d25ac9f94292877725c0eddae46d817211
Alyssa Ciurlik <aciurlik5@gmail.com>
2023-12-03 22:59:57 -0700

    registered elements

--- src/component/line-graph.js
+++ src/component/line-graph.js
@@ -2,29 +2,30 @@
 
 import React from "react";
 import { Line } from "react-chartjs-2";
+import {LinearScale, CategoryScale, Chart, PointElement, LineElement } from "chart.js";
+
+Chart.register(CategoryScale);
+Chart.register(LinearScale);
+Chart.register(PointElement);
+Chart.register(LineElement);
 
 
+export default function LineChart({ data, label, lineTitle }) {
+  const graphData = {
+    labels: label,
+    datasets: [
+      {
+        label: lineTitle,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: data,
+      },
+    ],
+  };
 
-
-export default function LineChart ({data, label, lineTitle}) {
-
-    const graphData = {
-        labels: label,
-        datasets: [
-          {
-            label: lineTitle,
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
-            data: data,
-          },
-        ],
-      };
-      
   return (
     <div>
       <Line data={graphData} />
     </div>
   );
 };
-
-
